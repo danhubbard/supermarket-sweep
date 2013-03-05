0f5afa371940641f066441d7c03a045c1b4383b6
Dan Hubbard <danhubbard@Dans-MacBook-Pro.local>
2013-03-05 17:14:33 +0000

    Add first promo rule

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,30 +1,41 @@
 class Checkout
-  def initialize(basket)
-    @basket = basket
+  def initialize(basket, promos=[])
+    @basket, @promos = basket, promos
   end
   
   def process
     @scanned = []
     @basket.each { |item| self.scan(item) }
-    self.total(@scanned)
+    self.total(@scanned, @promos)
   end
   
   def scan(item)
     @scanned << item
-    puts item.name
   end
   
-  def total(scanned)
-    @total = 0
+  def total(scanned, promos)
+    @total = discount = 0
     scanned.each { |item| @total += item.price }
+    promos.each { |p| discount += p.apply_promo(scanned) }
+    puts (@total - discount).round(2)
   end
 end
 
 class Item
-  attr_reader :name, :price
+  attr_reader :code, :name, :price
     
   def initialize(code, name, price)
     @code, @name, @price = code, name, price
+  end
+end
+
+class PromotionalItem
+  def initialize(name, &block)
+    @name, @block = name, block
+  end
+  
+  def apply_promo(items)
+    @block.call(items)
   end
 end
 
@@ -36,6 +47,13 @@ end
 @i2 = Item.new(002, "Personalised cufflinks", 45.00)
 @i3 = Item.new(002, "Kids T-shirt", 19.95)
 
-@basket1 = [@i1, @i2]
+@promo1 = PromotionalItem.new('10% off over 60') do |items|
+  total = 0
+  items.each { |item| total += item.price }
+  total > 60 ? total * 0.1 : 0
+end
 
-co = Checkout.new(@basket1)
+@basket1 = [@i1, @i2, @i2]
+
+co = Checkout.new(@basket1, [@promo1])
+co.process()
